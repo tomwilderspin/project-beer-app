aaaca03345aa60a5386d5fa5ec764eb8f04b3000
tomwilderspin <tomwilderspin@gmail.com>
2017-05-16 22:17:19 +0100

    added root reducer

--- src/Map/Reducer.js
+++ src/Map/Reducer.js
@@ -4,7 +4,7 @@ import { RECEIVED_MAP_PINS } from './Actions';
 export default function map(state = {}, action) {
   switch(action.type) {
     case RECEIVED_MAP_PINS:
-      return Object.assign(state, {pins: action.pins});
+      return Object.assign(state, { pins: action.pins });
     default: return state;
   }
 }
