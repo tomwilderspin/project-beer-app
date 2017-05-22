3bad0eef1f4773af44ac7bf13dada568f50ae8ad
tomwilderspin <tomwilderspin@gmail.com>
2017-05-22 21:25:59 +0100

    fixed broken app test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './Store';
 import App from './App';
+
+
+const mockState = configureStore({
+    map: {
+      initLocation: { lat: 53.798179, long: -1.543627},
+      initZoom: 7,
+      hasKey: true,
+      key: 12345,
+      pins: {}
+    }
+});
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={mockState}>
+    <App />
+    </Provider>, div);
 });
